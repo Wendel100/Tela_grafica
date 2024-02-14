e1391d7bb66a0c3438f5144ad6023971a5a53df7
Renan <renanwendel100@gmail.com>
2024-02-13 22:32:26 -0300

    version 1.0

---
+++ frame_principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tela_grafica;
+
+/**
+ *
+ * @author darkr
+ */
+public class frame_principal {
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
